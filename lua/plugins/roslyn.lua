d68bd62fd9e6c5f4c81c9edfaf9aaa1bb56825cf
ferris <fmaghi@outlook.com>
2026-06-15 14:48:13 +0800

    Auto-commit: update config on exit (2026-06-15 14:48:13)

--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -16,19 +16,38 @@ return {
         and blink.get_lsp_capabilities() 
         or vim.lsp.protocol.make_client_capabilities()
 
-      require("roslyn").setup({
+      local user_config = {
         args = {
           "--logLevel=Information",
           "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
         },
         config = {
           name = "roslyn", -- Explicitly name the server instance to satisfy _transport.lua
+          on_init = function(client)
+            if type(client.config.root_dir) == "function" then
+              client.config.root_dir = client.root_dir
+            end
+          end,
           on_attach = function(client, bufnr)
             -- Your LSP keymaps here
           end,
           capabilities = capabilities,
         },
-      })
+      }
+
+      require("roslyn").setup(user_config)
+      vim.lsp.config("roslyn", user_config.config)
+
+      -- If we are in a C# solution, start the LSP immediately on the current startup buffer
+      if has_sln then
+        vim.schedule(function()
+          local config = vim.lsp.config["roslyn"]
+          if config then
+            local bufnr = vim.api.nvim_get_current_buf()
+            vim.lsp.start(config, { bufnr = bufnr })
+          end
+        end)
+      end
     end,
   },
 }
