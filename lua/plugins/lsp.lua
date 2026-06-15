f6ff980e9b6a7d02f5accca153b433b3919773c3
ferris <fmaghi@outlook.com>
2026-06-15 15:30:54 +0800

    Auto-commit: update config on exit (2026-06-15 15:30:54)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,6 +7,11 @@ return {
       },
       servers = {
         roslyn_ls = { enabled = false },
+        copilot = {
+          cmd_env = {
+            XDG_CONFIG_HOME = vim.fn.stdpath("config") .. "/copilot",
+          },
+        },
       },
     },
   },
