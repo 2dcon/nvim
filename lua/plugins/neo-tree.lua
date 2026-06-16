208823cff03c7e28cb6d5de13d867980ef4fcb6c
ferris <fmaghi@outlook.com>
2026-06-16 11:54:01 +0800

    Auto-commit: update config on exit (2026-06-16 11:54:01)

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,6 +2,38 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      window = {
+        mappings = {
+          ["Y"] = function(state)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            local filename = node.name
+            local modify = vim.fn.fnamemodify
+            
+            local results = {
+              filepath,                     -- Absolute path
+              modify(filepath, ":."),      -- Relative to CWD
+              filename                     -- Filename only
+            }
+
+            vim.ui.select({
+              "1. Absolute path: " .. results[1],
+              "2. Path relative to CWD: " .. results[2],
+              "3. Filename: " .. results[3],
+            }, {
+              prompt = "Choose to copy to clipboard:",
+            }, function(choice)
+              if choice then
+                local idx = tonumber(choice:sub(1, 1))
+                if idx and results[idx] then
+                  vim.fn.setreg("+", results[idx])
+                  vim.notify("Copied: " .. results[idx])
+                end
+              end
+            end)
+          end,
+        },
+      },
       filesystem = {
         filtered_items = {
           visible = true, -- Show hidden files/folders by default (dimmed)
