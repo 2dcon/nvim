a185c1be7f4dc83da2552ee507086c1a3c7def03
ferris <fmaghi@outlook.com>
2026-06-16 13:03:45 +0800

    Auto-commit: update config on exit (2026-06-16 13:03:45)

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,24 @@
+return {
+  -- Configure Tokyonight with the "night" style
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night", -- Set style to "night" (much darker than storm)
+      transparent = false,
+      styles = {
+        sidebars = "dark",
+        floats = "dark",
+      },
+    },
+  },
+
+  -- Set active colorscheme to tokyonight-night
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+}
